45f8e2554e77665864256cb2fe84b496a9b208a4
[Ramsey Foster} <[ramsey.i.foster@gmail.com]>
2025-02-12 11:02:34 -0800

    DataBase_Test

---
+++ .history/firebase_20250212095648.js
@@ -0,0 +1,22 @@
+// Before running locally, install firebase with "npm install firebase"
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA8krcjPPhxF222gKTBUJhgeDuJ_6X5HiE",
+  authDomain: "cs-4800-in-construction-63b73.firebaseapp.com",
+  projectId: "cs-4800-in-construction-63b73",
+  storageBucket: "cs-4800-in-construction-63b73.firebasestorage.app",
+  messagingSenderId: "430628626450",
+  appId: "1:430628626450:web:a55ef8c6768ddb915e38cb",
+  measurementId: "G-CRXZ621DDX"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);

bc2de4a634570794038415fcf32a5ab5be754065
Christopher <ckurdoghlian@gmail.com>
2025-03-09 12:03:37 -0700

    fixed voting buttons for post page.

--- app/communities/[id]/users/page.tsx
+++ app/communities/[id]/users/page.tsx
@@ -157,7 +157,7 @@ export default function CommunityUsersPage() {
     <div className="min-h-screen flex bg-[var(--background)]">
       <MainNavbar user={user} />
       
-      <div className="flex-1 ml-64 flex flex-col min-h-screen bg-[var(--background)]">
+      <div className="flex-1 ml-6 flex flex-col min-h-screen bg-[var(--background)]">
         <main className="flex-grow p-6">
           <div className="max-w-4xl mx-auto">
             {/* Back button and navigation */}
